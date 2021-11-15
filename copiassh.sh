8c4cd3107f69cd8b1c3b20e24396b75e30e525c8
EndikaEiros <90617662+EndikaEiros@users.noreply.github.com>
2021-11-15 20:04:55 +0100

    Arreglado

--- copiassh.sh
+++ copiassh.sh
@@ -1,37 +1,42 @@
-user= "endika.eiros"
-ip= "35.216.168.201"
 
-dir=""                  #DIRECCION DE COPIA COMPLETA
-dirssh=""               #DIRECCION DONDE SE ALMACENA EN EL REMOTO
-compare=""              #OPCION DE --compane-dest del comando scp
+#read -p "Mete la IP" ip
+#read -p "Mete el usuario" usuario
+usuario="endika.eiros"
+ip="35.216.168.201"
+
+dir="/home/endika/Escritorio/seguridad"                  #DIRECCION DE COPIA COMPLETA
+dirssh="/home/endika.eiros/backup"               #DIRECCION DONDE SE ALMACENA EN EL REMOTO
+compare="/home/endika/Escritorio/copia"              #OPCION DE --compane-dest del comando scp Ultima copia completa
 
 function completasubir(){
-    rsync -avh $dir $user"@"$ip":"$dirssh
+    sudo rsync -avh $dir $usuario"@"$ip":"$dirssh
     exit 0
 }
 
 function incrementalsubir(){
-    mkdir copia
-    rsync -avh --compare-dest=$compare $dir $pwd"/copia"
-    scp $dir $user"@"$ip":"$dirssh
-    rm -r copia
+ 
+    rsync -avh --compare-dest=$compare $dir $compare"/incremental"
+    scp -r $compare"/incremental" $usuario"@"$ip":"$dirssh"/incremental"
+    #rm -r copia
     exit 0
 }
 
 function completa(){
-    echo "se guarda en " $pwd
-    rsync -avh $dir $pwd
+    echo "se guarda en " $compare
+    mkdir copia
+    sudo rsync -avh $dir $compare
     exit 0
 }
 
 function incremental(){
-    mkdir copia
-    rsync -avh --compare-dest=$compare $dir $pwd"/copia"
+    mkdir copia/incremental
+    rsync -avh --compare-dest=$compare $dir $compare"/incremental"
     exit 0
 }
 
-function subir(){
-    scp $dir $user"@"$ip":"$dirssh
+function subirincremental(){
+    echo "Se entiende que ha realizado la funcion 4 antes"
+    scp $compare"/incremental" $usuario"@"$ip":"$dirssh
     exit 0
 }
 
@@ -39,9 +44,10 @@ function subir(){
 
 
 function main(){
-    opcion=0
+    opcion=-1
     echo -e "introduce el numero de la funcion que se va a realizar "
-    while opcion -ne 0 do
+    while test $opcion -ne 0 
+    do
         echo -e "0 - Salir"
         echo -e "1 - Generar copia completa y generar backup mediante ssh"
         echo -e "2 - Generar copia incremental y subirlo por ssh"
@@ -53,14 +59,14 @@ function main(){
         read -p "Elige una opcion: " opcion
 
 
-    case $opcion in
+    	case $opcion in
 
-   		1) completasubir;;
-        2) incrementalsubir;;
-        3) completa;;
-        4) incremental;;
-        5) subir;;
-        *) ;;
+            1) completasubir;;
+            2) incrementalsubir;;
+            3) completa;;
+            4) incremental;;
+            5) subirincremental;;
+            *) ;;
 
     	esac
       
