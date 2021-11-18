24fc6fc6df37ea98669e5242b8d08e51dcdf4517
EndikaEiros <90617662+EndikaEiros@users.noreply.github.com>
2021-11-18 19:16:17 +0100

    Add files via upload

---
+++ ggp.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#https://www.gnupg.org/gph/en/manual/x56.html
+ssh(){
+    ssh-keygen -t rsa
+    cat ~/.ssh/id_rsa.pub > ssh.txt
+}
+
+#1) Anadirclave
+anadirclave(){
+    gpg --import ARCHIVO.asc
+}
+
+
+#2) verificar
+verificar(){
+    gpg --verify ARCHIVO.asc
+}
+
+
+#3) Crear clave
+crearclave(){
+  gpg --gen-key
+
+}
+
+#4) CifraryFirmar 
+CifraryFirmar(){
+    gpg --encrypt --sign -r correo archivo
+}
+5) encriptarClace
+encriptarClace(){
+    gpg -c archivo
+}
+
+#SALIR
+function fin()
+{
+	echo -e "¿Quieres salir del programa?(S/N)\n"
+        read respuesta
+	if [ $respuesta == "N" ] then
+		opcionmenuppal=0
+	else
+    exit 0
+  fi
+}
+
+### Main ###
+function main(){
+
+    opcionmenuppal=0
+    while test $opcionmenuppal -ne 99
+    do
+    	#Muestra el menu
+        echo -e "1) anadirclave "
+        echo -e "2) verificar "
+        echo -e "3) crearclave    "
+        echo -e "4) habilitarCambios    "
+	   echo -e "9) Fin  "
+        
+        echo ""
+        read -p "Elige una opcion: " opcionmenuppal
+                
+
+    	case $opcionmenuppal in
+
+            1) anadirclave;;
+            2) verificar;;
+            3) crearclave;;
+            4) habilitarCambios;;
+            5) fin;;
+		
+		
+		  6) fin;;
+        *) ;;
+
+    	esac
+
+    done
+
+}
+main
